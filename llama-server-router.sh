ce37d4a866633064900ff9d05f3ef9b77f39990f
Nicholas Burr <nbburr@gmail.com>
2026-05-07 15:38:35 -0400

    pushing local

--- llama-server-router.sh
+++ llama-server-router.sh
@@ -6,6 +6,6 @@ podman run --replace -itd --name llama-server \
 --group-add video --group-add render \
 --security-opt seccomp=unconfined \
 --publish 8080:8080 --ipc=host \
-localhost/llama.cpp:server llama-server --models-preset /root/.config/llama.cpp/models.ini --models-dir /root/.cache/huggingface/hub/ --log-timestamps --no-mmap 
+localhost/llama.cpp:server llama-server --models-preset /root/.config/llama.cpp/models.ini --models-dir /root/.cache/huggingface/hub/ --log-timestamps --no-mmap --host 0.0.0.0 --port 8080 --fit off 
 
 #localhost/llama.cpp:server llama-server --models-dir /root/.cache/huggingface/hub/ --log-timestamps --host 0.0.0.0 --port 8080 --no-mmap -fit off -c 8192 -ngl 999 -fa 1
