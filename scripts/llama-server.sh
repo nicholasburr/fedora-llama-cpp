4e4dc52970a4f7f65a2578ba25cd4a5a47abce49
Nicholas Burr <nbburr@gmail.com>
2026-06-03 13:36:33 -0400

    cleaned up old containers + scripts

--- scripts/llama-server.sh
+++ scripts/llama-server.sh
@@ -10,10 +10,11 @@ podman run --replace -itd --name llama-server \
   --security-opt=seccomp=unconfined \
   --secret=huggingface-token,type=env,target=HF_TOKEN \
   --volume=/home/nburr/.cache/huggingface/hub/:/root/.cache/huggingface/hub/:Z \
-  localhost/llama.cpp:server \
+  localhost/llama-server:rocm-7.2.4 \
     llama-server \
       --models-dir /root/.cache/huggingface/hub/ \
       --log-timestamps \
+      --threads 8 \
       --no-mmap \
       --host 0.0.0.0 \
       --port 8001 \
@@ -24,10 +25,11 @@ podman run --replace -itd --name llama-server \
       --parallel 2 \
       --cont-batching \
       --webui-mcp-proxy \
-      --jinja \
-      --preserve-thinking \ 
-      --spec-type draft-mtp \
-      --spec-draft-n-max 2
+      --jinja
+#      --spec-type draft-mtp \
+#ghcr.io/ggml-org/llama.cpp:server-rocm
+#      --spec-draft-n-max 2 \
+#      -np 1
 #--models-preset /root/.config/llama.cpp/models.ini
 #--volume=/home/nburr/.config/llama.cpp/:/root/.config/llama.cpp/:Z \
 #localhost/llama.cpp:server llama-server --models-dir /root/.cache/huggingface/hub/ --log-timestamps --host 0.0.0.0 --port 8080 --no-mmap -fit off -c 8192 -ngl 999 -fa 1
--- scripts/llama.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#podman run --replace -itd --name llama-server --volume=/home/nburr/.cache/huggingface/hub/:/root/.cache/huggingface/hub/:Z --device /dev/dri:/dev/dri --device /dev/kfd:/dev/kfd --group-add video --group-add render --security-opt seccomp=unconfined -p 8080:8080 --ipc=host localhost/llama.cpp:server llama-server -hf unsloth/gemma-4-31B-it-GGUF:BF16 --log-timestamps --host 0.0.0.0 --port 8080 --no-mmap -fit off -c 8192 -ngl 999 -fa 1
-
-#podman run --replace -itd --name llama-server --volume=/home/nburr/.cache/huggingface/hub/:/root/.cache/huggingface/hub/:Z --device /dev/dri:/dev/dri --device /dev/kfd:/dev/kfd --group-add video --group-add render --security-opt seccomp=unconfined -p 8080:8080 --ipc=host ghcr.io/ggml-org/llama.cpp:server-rocm -hf unsloth/gemma-4-31B-it-GGUF:BF16 --log-timestamps --host 0.0.0.0 --port 8080 --no-mmap -fit off -c 16384 -ngl 999 -fa 1
-
-#podman run --replace -itd --name llama-server --volume=/home/nburr/.cache/huggingface/hub/:/root/.cache/huggingface/hub/:Z --device /dev/dri:/dev/dri --device /dev/kfd:/dev/kfd --group-add video --group-add render --security-opt seccomp=unconfined -p 8080:8080 --ipc=host kyuz0/amd-strix-halo-toolboxes:rocm-7.2.2 llama-server -hf unsloth/gemma-4-31B-it-GGUF:BF16 --log-timestamps --host 0.0.0.0 --port 8080 --no-mmap -fit off -c 16384 -ngl 999 -fa 1
-
-
-podman run --replace -itd --name llama-server --volume=/home/nburr/.cache/huggingface/hub/:/root/.cache/huggingface/hub/:Z --device /dev/dri:/dev/dri --device /dev/kfd:/dev/kfd --group-add video --group-add render --security-opt seccomp=unconfined -p 8080:8080 --ipc=host localhost/llama.cpp:server llama-server -hf unsloth/Qwen3.6-35B-A3B-GGUF:UD-Q4_K_M --log-timestamps --host 0.0.0.0 --port 8080 --no-mmap -fit off -c 16382 -ngl 999 -fa 1
